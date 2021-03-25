27dfeca058443062312cc10a62ee4c2ecdb7eec7
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-03-25 14:00:32 +0800

    Add event hooks for watchRun and format the console.log messages
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,21 +9,27 @@ const validateEvent = (event) => {
 
 class WebpackWebhooksPlugin {
   constructor({ options, events }) {
-    this.options = {};
-    this.options.proxy = options.proxy || {};
+    this.options = {
+      proxy: options.proxy || {},
+      timeout: options.timeout || 0,
+    };
 
-    this.events = {};
-    this.events.beforeRun = validateEvent(events.beforeRun);
-    this.events.success = validateEvent(events.success);
-    this.events.error = validateEvent(events.error);
+    this.events = {
+      beforeRun: validateEvent(events.beforeRun),
+      watchRun: validateEvent(events.watchRun),
+      success: validateEvent(events.success),
+      error: validateEvent(events.error),
+    };
 
     this.beforeRunHook = this.beforeRunHook.bind(this);
+    this.watchRunHook = this.watchRunHook.bind(this);
     this.doneHook = this.doneHook.bind(this);
     this.post = this.post.bind(this);
   }
 
   apply(compiler) {
     compiler.hooks.beforeRun.tapAsync('WebpackWebhooksPlugin', this.beforeRunHook);
+    compiler.hooks.watchRun.tapAsync('WebpackWebhooksPlugin', this.watchRunHook);
     compiler.hooks.done.tapAsync('WebpackWebhooksPlugin', this.doneHook);
   }
 
@@ -31,19 +37,34 @@ class WebpackWebhooksPlugin {
     const config = {
       ...event,
       proxy: this.options.proxy,
+      timeout: this.options.timeout,
       method: 'POST',
     };
 
     try {
       const response = await axios(config);
-      console.log(response.data.json);
+      console.log(
+        '\x1b[32m%s\x1b[0m %s',
+        '[Webhooks]', // Text color: Green
+        `${config.url} [Status: ${response.status} ${response.statusText}]`,
+      );
     } catch (error) {
-      console.log(error);
+      console.log(
+        '\x1b[31m%s\x1b[0m %s',
+        '[Webhooks]', // Text color: Red
+        `${config.url} [Message: ${error.message}]`,
+      );
     }
   }
 
   async beforeRunHook(compiler, callback) {
     const promiseArray = this.events.beforeRun.map((event) => this.post(event));
+    await Promise.allSettled(promiseArray);
+    callback();
+  }
+
+  async watchRunHook(compiler, callback) {
+    const promiseArray = this.events.watchRun.map((event) => this.post(event));
     await Promise.allSettled(promiseArray);
     callback();
   }
